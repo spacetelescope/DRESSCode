7f1ba53187351de1f8399f89b35654e41904de9d
Ben Falk <bfalk@stsci.edu>
2021-04-28 17:40:57 -0400

    add progress bar
    move code into functions
    
    all process pool to run in parallel
    
    switch to use pathlib
    
    close header file w/ context manager

--- uvotimage.py
+++ uvotimage.py
@@ -1,43 +1,44 @@
 """ Script to create sky images from raw images and event files. """
 
-import os
 import subprocess
 from multiprocessing import Pool
+from pathlib import Path
+from typing import List, Sequence
 
+from tqdm import tqdm
 from astropy.io import fits
 
 import configloader
 
-config = configloader.load_config()
 
-# Specify the galaxy and the path to the working directory.
-galaxy = config["galaxy"]
-path = config["path"] + galaxy + "/working_dir/"
+def get_filenames(extensions: Sequence[str]) -> List[Path]:
+    """ Return filenames from the  working directory and the path to the working directory """
 
+    config = configloader.load_config()
+    galaxy = config["galaxy"]
+    working_dir_path = Path(config["path"] + galaxy + "/working_dir/")
 
-print("Creating sky images...")
+    paths = []
+    for ext in extensions:
+        paths += list(working_dir_path.glob(ext))
 
-# For all files in the working directory:
-filenames = [
-    filename
-    for filename in sorted(os.listdir(path))
-    if filename.endswith("rw.img") or filename.endswith(".evt")
-]
-num = len(filenames)
+    # todo: add exclude patterns
+    return paths
 
 
-def process_files(filename, i):
+def process_files(filename: Path):
+    """ process a full filepath """
 
     error = False
 
     # Specify the input file, the prefix for the output file, the attitude file and the terminal output file.
-    infile = filename
-    prefix = filename.split("u")[0] + "_" + filename.split(".")[1] + "_"
-    attfile = filename.split("u", 1)[0] + "pat.fits"
-    terminal_output_file = path + "output_uvotimage_" + filename.split(".")[0] + ".txt"
+    prefix = filename.name.split("u")[0] + "_" + filename.name.split(".")[1] + "_"
+    attfile = filename.parent / "{}pat.fits".format(filename.name.split("u", 1)[0])
+    terminal_output_file = filename.parent / f"output_uvotimage_{filename.stem}.txt"
 
     # Open the file and take the RA, DEC and roll from the header.
-    header = fits.open(path + filename)[0].header
+    with fits.open(filename) as hdul:
+        header = hdul[0].header
     ra = header["RA_PNT"]
     dec = header["DEC_PNT"]
     pa = header["PA_PNT"]
@@ -49,7 +50,7 @@ def process_files(filename, i):
             subprocess.run(
                 (
                     "uvotimage"
-                    f" infile={infile}"
+                    f" infile={filename}"
                     f" prefix={prefix}"
                     f" attfile={attfile}"
                     " teldeffile=CALDB alignfile=CALDB"
@@ -58,37 +59,77 @@ def process_files(filename, i):
                     f" roll={pa}"
                     " mod8corr=yes refattopt='ANGLE_d=5,OFFSET_s=1000'"
                 ),
-                cwd=path,
+                cwd=filename.parent,
                 shell=True,
                 stdout=terminal,
                 check=True,
             )
         except subprocess.CalledProcessError:
             error = True
-
-    # todo: do validation outside this function, so print statements present in order
-    # Check if the sky image was successfully created.
-    with open(terminal_output_file, "r") as file:
-        for line in file:
-            # If the word "error" is encountered, print an error message.
-            if "error" in line:
-                print("An error has occured for image " + filename)
-                error = True
-
-            # If uvotimage skipped an event based image HDU, let the user know.
-            if "skipping event based image HDU" in line:
-                print(line, " in file " + filename)
-
-    # Print user information.
-    print(f"Sky image created for all (other) frames of {filename} ({i})/{num})")
+            print(f"error on file: {filename.name}")
 
     return error
 
 
-with Pool() as p:
-    errors = p.starmap(process_files, zip(filenames, range(len(filenames))))
+def validate_terminal_output(filenames: List[Path]):
+
+    num_files = len(filenames)
+    errors = []
+
+    for index, filename in enumerate(filenames):
+
+        error = False
+
+        terminal_output_file = filename.parent / f"output_uvotimage_{filename.stem}.txt"
+
+        if not terminal_output_file.exists():
+            continue
+
+        # todo: do validation outside this function, so print statements present in order
+        # Check if the sky image was successfully created.
+        with open(terminal_output_file, "r") as file:
+            for line in file:
+                # If the word "error" is encountered, print an error message.
+                if "error" in line:
+                    print("An error has occured for image " + filename.name)
+                    error = True
+
+                # If uvotimage skipped an event based image HDU, let the user know.
+                if "skipping event based image HDU" in line:
+                    print(line, " in file " + filename.name)
+
+        # Print user information.
+        print(
+            f"Sky image created for all (other) frames of {filename.name} ({index})/{num_files})"
+        )
+        errors.append(error)
+
+    return errors
 
 
-# Print user information.
-if not any(errors):
-    print("Sky images were successfully created for all raw images and event files.")
+def main():
+
+    filenames = get_filenames(["*rw.img", "*.evt"])
+
+    print("Creating sky images...")
+
+    errors = []
+
+    #! error: "can't stat user parameter file .../sec2time.par" when running in parallel
+
+    with Pool() as p:
+        for result in tqdm(p.imap(process_files, filenames), total=len(filenames)):
+            errors.append(result)
+
+    print("validating results")
+    errors_validation = validate_terminal_output(filenames)
+
+    # Print user information.
+    if not any(errors) and not any(errors_validation):
+        print(
+            "Sky images were successfully created for all raw images and event files."
+        )
+
+
+if __name__ == "__main__":
+    main()
