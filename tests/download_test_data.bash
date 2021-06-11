9522aeff83ba0108ff7c60249188b9dc26dc7cc0
Ben Falk <bfalk@stsci.edu>
2021-06-11 14:38:21 -0400

    reduce test data set

--- tests/download_test_data.bash
+++ tests/download_test_data.bash
@@ -5,15 +5,15 @@ set -e
 OUTPUT_DIR=$1
 
 # https://heasarc.gsfc.nasa.gov/cgi-bin/W3Browse/swift.pl
-# parameter search form
+# UVOT Log -> parameter search form
 # filter: 'UVW2' OR 'UVM2' OR 'UVW1'
 # pointing_mode: POINTING
 # Object Name: NGC0628
-# obsid's: 00032891001;00032891019;00035868001;00036568001;00036568002;00036568003
+# obsid's: 00032891004 OR 00032891012 OR 00035868001
 
 echo "Downloading raw data to $OUTPUT_DIR"
 
-for obs in "2007_07/00036568001/" "2008_02/00035868001/" "2015_06/00036568002/" "2013_07/00032891001/" "2013_08/00032891019/" "2015_07/00036568003/"
+for obs in "2008_02/00035868001/" "2013_08/00032891004/" "2013_08/00032891012/"
 do
   echo "Downloading obs: $obs"
   wget -P "$OUTPUT_DIR" -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R "index*" -erobots=off --retr-symlinks "https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/$obs"
