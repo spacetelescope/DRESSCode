e92a0a17c4088ab7ae601ddc49e89c14530366d8
Ben Falk <bfalk@stsci.edu>
2022-02-17 18:10:55 -0500

    wip: uvotimsum
    - how to append already corrected images?

--- dresscode/uvotimsum.py
+++ dresscode/uvotimsum.py
@@ -18,7 +18,13 @@ from typing import Optional, Sequence
 import numpy as np
 from astropy.io import fits
 
-from dresscode.utils import load_config
+from dresscode.utils import load_config, check_filter
+
+FILE_PATT_TO_SUM = {
+    "_sk_corr_coi_lss_zp.img": "sk",
+    "_mk_corr.img": "mk",
+    "_ex_corr.img": "ex",
+}
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -33,135 +39,126 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     # Specify the galaxy and the path to the working directory.
     galaxy = config["galaxy"]
     path = config["path"] + galaxy + "/working_dir/"
-    years = config["years"]
 
-    # Loop over the different years.
-    for year in years:
+    # todo: what files are we supposed to sum here? -- @mdecleir
+    # orig: *sk_corr.img, *ex_corr.img, *lss_corr.img, *mk_corr.img
 
-        print("Year: " + year)
-        yearpath = path + year + "/"
+    # since lss_corr images have already been used for applying corrections per observation... no longer needed
 
-        # PART 1: Append all frames per filter and per type.
+    # new: *_sk_corr_coi_lss_zp.img, *_mk_corr.img, *_ex_corr.img
 
-        print("Appending all frames...")
+    files_to_sum = [
+        filename
+        for filename in sorted(os.listdir(path))
+        if filename.endswith(tuple(FILE_PATT_TO_SUM.keys()))
+    ]
 
-        for i, filename in enumerate(sorted(os.listdir(yearpath))):
+    # PART 1: Append all frames per filter and per type.
 
-            # Check the type of the image and give the image a type label.
-            typelabel = check_type(filename)
+    print("Appending all frames...")
 
-            if typelabel is None:
-                continue
+    for i, filename in enumerate(files_to_sum):
 
-            # For the mask files: make sure that also the NaN pixels in the exposure
-            # maps are included in the mask as well as pixels with a very low exposure
-            # time.
-            if typelabel == "mk":
-                update_mask(yearpath + filename)
-                filename = filename.replace(".img", "_new.img")
+        # Check the type of the image and give the image a type label.
+        typelabel = check_type(filename)
 
-            # Check the filter of the image and give the image a filter label.
-            filterlabel = check_filter(filename)
+        if typelabel is None:
+            continue
 
-            # Append all frames to one "total" image.
-            append(yearpath + filename, typelabel, filterlabel, i)
+        # For the mask files: make sure that also the NaN pixels in the exposure
+        # maps are included in the mask as well as pixels with a very low exposure
+        # time.
+        if typelabel == "mk":
+            update_mask(path + filename)
+            filename = filename.replace(".img", "_new.img")
 
-        # PART 2: Co-add the frames in each "total" image.
+        # Check the filter of the image and give the image a filter label.
+        filterlabel = check_filter(filename)
 
-        print("Co-adding all frames...")
+        # Append all frames to one "total" image.
+        append(path + filename, typelabel, filterlabel)
 
-        if os.path.isfile(yearpath + "all_um2_sk.img"):
-            coaddframes(yearpath + "all_um2_sk.img", "grid")
-        if os.path.isfile(yearpath + "all_uw2_sk.img"):
-            coaddframes(yearpath + "all_uw2_sk.img", "grid")
-        if os.path.isfile(yearpath + "all_uw1_sk.img"):
-            coaddframes(yearpath + "all_uw1_sk.img", "grid")
+        print(f"Finished appending frames for image {i+1}/{len(files_to_sum)}.")
 
-        if os.path.isfile(yearpath + "all_um2_ex.img"):
-            coaddframes(yearpath + "all_um2_ex.img", "expmap")
-        if os.path.isfile(yearpath + "all_uw2_ex.img"):
-            coaddframes(yearpath + "all_uw2_ex.img", "expmap")
-        if os.path.isfile(yearpath + "all_uw1_ex.img"):
-            coaddframes(yearpath + "all_uw1_ex.img", "expmap")
+    # PART 2: Co-add the frames in each "total" image.
 
-        if os.path.isfile(yearpath + "all_um2_lss.img"):
-            coaddframes(yearpath + "all_um2_lss.img", "lssmap")
-        if os.path.isfile(yearpath + "all_uw2_lss.img"):
-            coaddframes(yearpath + "all_uw2_lss.img", "lssmap")
-        if os.path.isfile(yearpath + "all_uw1_lss.img"):
-            coaddframes(yearpath + "all_uw1_lss.img", "lssmap")
+    print("Co-adding all frames...")
 
-        # Check the output of the uvotimsum task.
-        error = False
+    if os.path.isfile(path + "all_um2_sk.img"):
+        coaddframes(path + "all_um2_sk.img", "grid")
+    if os.path.isfile(path + "all_uw2_sk.img"):
+        coaddframes(path + "all_uw2_sk.img", "grid")
+    if os.path.isfile(path + "all_uw1_sk.img"):
+        coaddframes(path + "all_uw1_sk.img", "grid")
 
-        for filename in sorted(os.listdir(yearpath)):
+    if os.path.isfile(path + "all_um2_ex.img"):
+        coaddframes(path + "all_um2_ex.img", "expmap")
+    if os.path.isfile(path + "all_uw2_ex.img"):
+        coaddframes(path + "all_uw2_ex.img", "expmap")
+    if os.path.isfile(path + "all_uw1_ex.img"):
+        coaddframes(path + "all_uw1_ex.img", "expmap")
 
-            # If the file is an output text file of uvotimsum, open the file.
-            if filename.startswith("output_uvotimsum"):
-                with open(yearpath + filename, "r") as fh:
-                    text = fh.read()
+    # Check the output of the uvotimsum task.
+    error = False
 
-                # If the word "error" is encountered, print an error message.
-                if (
-                    "error" in text
-                    or "created output image" not in text
-                    or "all checksums are valid" not in text
-                ):
-                    print(
-                        "An error has occurred for image all_"
-                        + filename.split("_")[2]
-                        + "_"
-                        + filename.split("_")[3].split(".")[0]
-                        + ".img"
-                    )
-                    error = True
+    output_ovitimsum_files = [
+        filename
+        for filename in sorted(os.listdir(path))
+        if filename.startswith("output_uvotimsum")
+    ]
 
-        # PART 3: Normalize the summed sky images.
+    for filename in output_ovitimsum_files:
 
-        print("Normalizing the summed sky images...")
+        # If the file is an output text file of uvotimsum, open the file.
+        with open(path + filename, "r") as fh:
+            text = fh.read()
 
-        if os.path.isfile(yearpath + "sum_um2_sk.img"):
-            norm(yearpath + "sum_um2_sk.img")
-        if os.path.isfile(yearpath + "sum_uw2_sk.img"):
-            norm(yearpath + "sum_uw2_sk.img")
-        if os.path.isfile(yearpath + "sum_uw1_sk.img"):
-            norm(yearpath + "sum_uw1_sk.img")
-
-        if error is False:
+        # If the word "error" is encountered, print an error message.
+        if (
+            "error" in text
+            or "created output image" not in text
+            or "all checksums are valid" not in text
+        ):
             print(
-                "All frames were successfully co-added and the summed sky images were "
-                "normalized."
+                "An error has occurred for image all_"
+                + filename.split("_")[2]
+                + "_"
+                + filename.split("_")[3].split(".")[0]
+                + ".img"
             )
+            error = True
+
+    # PART 3: Normalize the summed sky images.
+
+    print("Normalizing the summed sky images...")
+
+    if os.path.isfile(path + "sum_um2_sk.img"):
+        norm(path + "sum_um2_sk.img")
+    if os.path.isfile(path + "sum_uw2_sk.img"):
+        norm(path + "sum_uw2_sk.img")
+    if os.path.isfile(path + "sum_uw1_sk.img"):
+        norm(path + "sum_uw1_sk.img")
+
+    if error is False:
+        print(
+            "All frames were successfully co-added and the summed sky images were "
+            "normalized."
+        )
 
     return 0
 
 
 # Functions for PART 1: Appending frames.
-# Function to check the type of the image and return a type label.
 def check_type(filename):
-    if filename.endswith("sk_corr.img"):
-        return "sk"
-    elif filename.endswith("ex_corr.img"):
-        return "ex"
-    elif filename.endswith("lss_corr.img"):
-        return "lss"
-    elif filename.endswith("mk_corr.img"):
-        return "mk"
+    """check the type of the image and return a type label"""
+    for file_patt, filetype in FILE_PATT_TO_SUM.items():
+        if filename.endswith(file_patt):
+            return filetype
 
 
-# Function to check the filter of the image and return a filter label.
-def check_filter(filename):
-    if "um2" in filename:
-        return "um2"
-    elif "uw2" in filename:
-        return "uw2"
-    elif "uw1" in filename:
-        return "uw1"
-
-
-# Function to update the mask with pixels that are NaN in the exposure map and pixels
-# that have very low exposure times.
 def update_mask(filename):
+    """update the mask with pixels that are NaN in the exposure map and pixels
+    that have very low exposure times."""
     # Open the mask file and the exposure map and copy the primary header (extension 0
     # of hdulist) to a new hdulist.
     hdulist_mk = fits.open(filename)
@@ -181,9 +178,9 @@ def update_mask(filename):
     new_hdulist.writeto(filename.replace(".img", "_new.img"))
 
 
-# Function to copy the first image of a certain filter and a certain type or to append
-# frames, depending on whether it is the first image or not.
-def append(filename, typelabel, filterlabel, i):
+def append(filename, typelabel, filterlabel):
+    """Copy the first image of a filter and type into new image
+    OR append frames, depending on whether it is the first image or not"""
     allfile = (
         os.path.dirname(filename) + "/all_" + filterlabel + "_" + typelabel + ".img"
     )
@@ -191,48 +188,33 @@ def append(filename, typelabel, filterlabel, i):
     # If the "total" image of this type and this filter does not yet exist, create it.
     if not os.path.isfile(allfile):
         shutil.copyfile(filename, allfile)
-
         print("File " + os.path.basename(allfile) + " has been created.")
-    # Else: append the frames.
     else:
-        appendframes(filename, allfile, i)
+        appendframes(filename, allfile)
 
 
-# Function to open an image and append all its frames to the "total" image.
-def appendframes(filename, allfile, i):
+def appendframes(filename, allfile):
+    """open an image and append all its frames to the "total" image"""
     path = os.path.dirname(filename) + "/"
 
     # Count the total number of images.
-    num = sum(
-        4 for filename in sorted(os.listdir(path)) if filename.endswith("sk_corr.img")
-    )
 
     hdulist = fits.open(filename)
     for j in range(1, len(hdulist)):
         infile = filename + "+" + str(j)
         totfile = allfile
+        # todo: sk images: how to append frames when frames have been corrected [data, corrfactor, coicorr_rel]
         subprocess.call("ftappend " + infile + " " + totfile, cwd=path, shell=True)
 
         print(
-            "Frame "
-            + os.path.basename(infile)
-            + " (frame "
-            + str(j)
-            + "/"
-            + str(len(hdulist) - 1)
-            + " of image "
-            + str(i)
-            + "/"
-            + str(num)
-            + ") appended to "
-            + os.path.basename(allfile)
-            + "."
+            f"Frame {os.path.basename(infile)}"
+            + f" (frame {j}/{len(hdulist) - 1}) appended to {os.path.basename(allfile)}."
         )
 
 
 # Function for PART 2: Co-add frames.
-# Function to co-add all frames of an image.
 def coaddframes(allfile, method):
+    """co-add all frames of an image"""
     path = os.path.dirname(allfile) + "/"
 
     # Specify the output file, the mask file and the terminal output file.
@@ -267,8 +249,8 @@ def coaddframes(allfile, method):
 
 
 # Function for PART 3: Normalizing images.
-# Function to normalize an image.
 def norm(filename):
+    """Function to normalize an image"""
     path = os.path.dirname(filename) + "/"
 
     # Specify the input files and the output file.
